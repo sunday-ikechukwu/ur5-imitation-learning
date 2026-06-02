2e1e06768e42e168c476388f1d01d9bda2d3f3d2
Precious_weal <preciousweal2000@gmail.com>
2026-06-02 11:48:06 +0100

    Export record_episode and get_handles
    Updated __init__.py to make these functions easily importable across the project.

--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -1,7 +1,8 @@
 '''This module contains utility functions for the project.'''
 
 from .zmq_remoteapi_connection_utils import connect_to_coppeliasim
-from .pick_and_place_imitation_data_recorder_utils import record_episode
+from .pick_and_place_imitation_data_recorder_utils import record_episode, get_handles
+from .randomize_cube_position_utils import randomize_cube_position
 
 # Optional: define what gets imported with "from utils import *"
-__all__ = ["connect_to_coppeliasim", "record_episode"]
+__all__ = ["connect_to_coppeliasim", "record_episode", "get_handles", "randomize_cube_position"]
